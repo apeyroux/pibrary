1d27371b7d1495e74fa215840deb53911cf8af0a
Alexandre Peyroux <alex@xn--wxa.email>
2017-12-24 23:10:56 +0100

    fst commit

---
+++ pibrary.py
@@ -0,0 +1,47 @@
+import json
+import sys
+from amazon.api import AmazonAPI
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class Livre(Base):
+    __tablename__ = 'livres'
+
+    id = Column(Integer, primary_key=True)
+    isbn = Column(String)
+    titre = Column(String)
+    auteur = Column(String)
+    prix = Column(Float)
+
+if __name__ == "__main__":
+
+    with open('aws.id.json', 'r') as cfg:
+        config = json.loads(cfg.read())
+
+    db = create_engine('sqlite:///livres.db')
+    Base.metadata.create_all(db)
+    Session = sessionmaker(bind=db)
+    session = Session()
+
+    amazon = AmazonAPI(config['AMAZON_ACCESS_KEY'], config['AMAZON_SECRET_KEY'], config['AMAZON_ASSOC_TAG'], region="FR")
+    amout = 0
+    while True:
+        try:
+            isbn = sys.stdin.readline()
+            products = amazon.search(Keywords=isbn.strip(), SearchIndex='All')
+            for _, product in enumerate(products):
+                print("{} de {} au prix de {}€".format(product.title, ', '.join(product.authors), product.price_and_currency[0]))
+                if product.price_and_currency[0] :
+                    if not session.query(Livre).filter_by(isbn=isbn).first() :
+                        l = Livre(isbn=isbn, titre=product.title, auteur=', '.join(product.authors), prix=product.price_and_currency[0])
+                        session.add(l)
+                        session.commit()
+                        amout = amout + product.price_and_currency[0]
+                        print("Dépense de {}€".format(amout))
+                    else:
+                        print("Déjà en db")
+        except Exception as ex:
+            print(ex)
---
+++ requirements.nix
@@ -0,0 +1,189 @@
+# generated using pypi2nix tool (version: 1.8.0)
+# See more at: https://github.com/garbas/pypi2nix
+#
+# COMMAND:
+#   pypi2nix -r requirements.txt -V 3 -E libxml2 -E libxslt
+#
+
+{ pkgs ? import <nixpkgs> {}
+}:
+
+let
+
+  inherit (pkgs) makeWrapper;
+  inherit (pkgs.stdenv.lib) fix' extends inNixShell;
+
+  pythonPackages =
+  import "${toString pkgs.path}/pkgs/top-level/python-packages.nix" {
+    inherit pkgs;
+    inherit (pkgs) stdenv;
+    python = pkgs.python3;
+  };
+
+  commonBuildInputs = with pkgs; [ libxml2 libxslt ];
+  commonDoCheck = false;
+
+  withPackages = pkgs':
+    let
+      pkgs = builtins.removeAttrs pkgs' ["__unfix__"];
+      interpreter = pythonPackages.buildPythonPackage {
+        name = "python3-interpreter";
+        buildInputs = [ makeWrapper ] ++ (builtins.attrValues pkgs);
+        buildCommand = ''
+          mkdir -p $out/bin
+          ln -s ${pythonPackages.python.interpreter}               $out/bin/${pythonPackages.python.executable}
+          for dep in ${builtins.concatStringsSep " "               (builtins.attrValues pkgs)}; do
+            if [ -d "$dep/bin" ]; then
+              for prog in "$dep/bin/"*; do
+                if [ -f $prog ]; then
+                  ln -s $prog $out/bin/`basename $prog`
+                fi
+              done
+            fi
+          done
+          for prog in "$out/bin/"*; do
+            wrapProgram "$prog" --prefix PYTHONPATH : "$PYTHONPATH"
+          done
+          pushd $out/bin
+          ln -s ${pythonPackages.python.executable} python
+          popd
+        '';
+        passthru.interpreter = pythonPackages.python;
+      };
+    in {
+      __old = pythonPackages;
+      inherit interpreter;
+      mkDerivation = pythonPackages.buildPythonPackage;
+      packages = pkgs;
+      overrideDerivation = drv: f:
+        pythonPackages.buildPythonPackage (drv.drvAttrs // f drv.drvAttrs);
+      withPackages = pkgs'':
+        withPackages (pkgs // pkgs'');
+    };
+
+  python = withPackages {};
+
+  generated = self: {
+
+    "SQLAlchemy" = python.mkDerivation {
+      name = "SQLAlchemy-1.1.15";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/c2/f6/11fcc1ce19a7cb81b1c9377f4e27ce3813265611922e355905e57c44d164/SQLAlchemy-1.1.15.tar.gz"; sha256 = "8b79a5ed91cdcb5abe97b0045664c55c140aec09e5dd5c01303e23de5fe7a95a"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [ ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "";
+        license = licenses.mit;
+        description = "Database Abstraction Library";
+      };
+    };
+
+
+
+    "bottlenose" = python.mkDerivation {
+      name = "bottlenose-1.1.3";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/49/ae/7f3aaeb43af855cebd8f3893fe067f8d75eac9d174322a94186a19987036/bottlenose-1.1.3.tar.gz"; sha256 = "127663bfe98d7ccefedf4b7157c33471d1a293a232c719d4874f679c3811b7b1"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [ ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "";
+        license = licenses.asl20;
+        description = "A Python hook into the Amazon.com Product Advertising API";
+      };
+    };
+
+
+
+    "click" = python.mkDerivation {
+      name = "click-6.7";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/95/d9/c3336b6b5711c3ab9d1d3a80f1a3e2afeb9d8c02a7166462f6cc96570897/click-6.7.tar.gz"; sha256 = "f15516df478d5a56180fbf80e68f206010e6d160fc39fa508b65e035fd75130b"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [ ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "";
+        license = licenses.bsdOriginal;
+        description = "A simple wrapper around optparse for powerful command line utilities.";
+      };
+    };
+
+
+
+    "lxml" = python.mkDerivation {
+      name = "lxml-4.1.1";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/e1/4c/d83979fbc66a2154850f472e69405572d89d2e6a6daee30d18e83e39ef3a/lxml-4.1.1.tar.gz"; sha256 = "940caef1ec7c78e0c34b0f6b94fe42d0f2022915ffc78643d28538a5cfd0f40e"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [ ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "";
+        license = licenses.bsdOriginal;
+        description = "Powerful and Pythonic XML processing library combining libxml2/libxslt with the ElementTree API.";
+      };
+    };
+
+
+
+    "python-amazon-simple-product-api" = python.mkDerivation {
+      name = "python-amazon-simple-product-api-2.2.11";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/48/e3/0c921384c46dbcb5d6eb897d60f30964c0466b2b19c1b46ab9dc1f9b5934/python-amazon-simple-product-api-2.2.11.tar.gz"; sha256 = "5a362125903d3b6409d437e5a2b72614fb79624799038e6561f31ed91a90fce0"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [
+      self."bottlenose"
+      self."lxml"
+      self."python-dateutil"
+    ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "";
+        license = licenses.asl20;
+        description = "A simple Python wrapper for the Amazon.com Product Advertising API";
+      };
+    };
+
+
+
+    "python-dateutil" = python.mkDerivation {
+      name = "python-dateutil-2.6.1";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/54/bb/f1db86504f7a49e1d9b9301531181b00a1c7325dc85a29160ee3eaa73a54/python-dateutil-2.6.1.tar.gz"; sha256 = "891c38b2a02f5bb1be3e4793866c8df49c7d19baabf9c1bad62547e0b4866aca"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [
+      self."six"
+    ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "";
+        license = licenses.bsdOriginal;
+        description = "Extensions to the standard Python datetime module";
+      };
+    };
+
+
+
+    "six" = python.mkDerivation {
+      name = "six-1.11.0";
+      src = pkgs.fetchurl { url = "https://pypi.python.org/packages/16/d8/bc6316cf98419719bd59c91742194c111b6f2e85abac88e496adefaf7afe/six-1.11.0.tar.gz"; sha256 = "70e8a77beed4562e7f14fe23a786b54f6296e34344c23bc42f07b15018ff98e9"; };
+      doCheck = commonDoCheck;
+      buildInputs = commonBuildInputs;
+      propagatedBuildInputs = [ ];
+      meta = with pkgs.stdenv.lib; {
+        homepage = "";
+        license = licenses.mit;
+        description = "Python 2 and 3 compatibility utilities";
+      };
+    };
+
+  };
+  overrides = import ./requirements_override.nix { inherit pkgs python; };
+  commonOverrides = [
+
+  ];
+
+in python.withPackages
+   (fix' (pkgs.lib.fold
+            extends
+            generated
+            ([overrides] ++ commonOverrides)
+         )
+   )
