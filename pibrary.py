3c6b812af4aa388a8b5e9fa2b3e977edbbefa9cb
Alexandre Peyroux <alex@xn--wxa.email>
2017-12-25 00:12:44 +0100

    beautify

--- pibrary.py
+++ pibrary.py
@@ -1,12 +1,14 @@
+import atexit
 import json
 import sys
 
 from amazon.api import AmazonAPI
-from sqlalchemy import *
+from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
+
 
 class Livre(Base):
     __tablename__ = 'livres'
@@ -17,7 +19,10 @@ class Livre(Base):
     auteur = Column(String)
     prix = Column(Float)
 
+
 if __name__ == "__main__":
+
+    atexit.register(lambda: print("Exit"))
 
     with open('aws.id.json', 'r') as cfg:
         config = json.loads(cfg.read())
@@ -27,26 +32,36 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=db)
     session = Session()
 
-    amazon = AmazonAPI(config['AMAZON_ACCESS_KEY'], config['AMAZON_SECRET_KEY'], config['AMAZON_ASSOC_TAG'], region="FR")
+    amazon = AmazonAPI(config['AMAZON_ACCESS_KEY'],
+                       config['AMAZON_SECRET_KEY'],
+                       config['AMAZON_ASSOC_TAG'],
+                       region="FR",
+                       )
     amout = 0
     print("Entrer le livre à ajouter (ISBN ou champ libre): ")
     while True:
         try:
             recherche = sys.stdin.readline()
-            products = amazon.search(Keywords=recherche.strip(), SearchIndex='All')
+            products = amazon.search(
+                Keywords=recherche.strip(), SearchIndex='All')
             for _, product in enumerate(products):
-                if product.price_and_currency[0] :
+                if product.price_and_currency[0]:
                     prix = product.price_and_currency[0]
                 else:
                     prix = 0
-                print("{} de {} au prix de {}€".format(product.title, ', '.join(product.authors), prix))
+                print("{} de {} au prix de {}€".format(
+                    product.title, ', '.join(product.authors), prix))
                 sys.stdout.write("Ajouter ?: [Y/n]")
                 reponse = input().lower()
                 if reponse == 'n':
                     print("Insertion annulée.")
                 elif reponse == 'y' or reponse == '':
-                    if not session.query(Livre).filter_by(isbn=product.isbn).first() :
-                        l = Livre(isbn=product.isbn, titre=product.title, auteur=', '.join(product.authors), prix=prix)
+                    if not session.query(Livre).filter_by(isbn=product.isbn).first():
+                        l = Livre(isbn=product.isbn,
+                                  titre=product.title, auteur=', '.join(
+                                      product.authors),
+                                  prix=prix,
+                                  )
                         session.add(l)
                         session.commit()
                         amout = amout + prix
@@ -57,5 +72,3 @@ if __name__ == "__main__":
                     print("Je ne comprend pas la réponse.")
         except Exception as ex:
             print(ex)
-
-    atexit.register(lambda : print("Exit"))
