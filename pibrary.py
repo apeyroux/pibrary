dfb8bef9f81283e5110009cf83ea340649859f03
Alexandre Peyroux <alex@xn--wxa.email>
2017-12-25 00:06:27 +0100

    recherche et Y/n

--- pibrary.py
+++ pibrary.py
@@ -1,5 +1,6 @@
 import json
 import sys
+
 from amazon.api import AmazonAPI
 from sqlalchemy import *
 from sqlalchemy.ext.declarative import declarative_base
@@ -28,24 +29,33 @@ if __name__ == "__main__":
 
     amazon = AmazonAPI(config['AMAZON_ACCESS_KEY'], config['AMAZON_SECRET_KEY'], config['AMAZON_ASSOC_TAG'], region="FR")
     amout = 0
-    print("ENTER ISBN:")
+    print("Entrer le livre à ajouter (ISBN ou champ libre): ")
     while True:
         try:
-            isbn = sys.stdin.readline()
-            products = amazon.search(Keywords=isbn.strip(), SearchIndex='All')
+            recherche = sys.stdin.readline()
+            products = amazon.search(Keywords=recherche.strip(), SearchIndex='All')
             for _, product in enumerate(products):
                 if product.price_and_currency[0] :
                     prix = product.price_and_currency[0]
                 else:
                     prix = 0
                 print("{} de {} au prix de {}€".format(product.title, ', '.join(product.authors), prix))
-                if not session.query(Livre).filter_by(isbn=isbn).first() :
-                    l = Livre(isbn=isbn, titre=product.title, auteur=', '.join(product.authors), prix=prix)
-                    session.add(l)
-                    session.commit()
-                    amout = amout + prix
-                    print("Dépense de {}€".format(amout))
+                sys.stdout.write("Ajouter ?: [Y/n]")
+                reponse = input().lower()
+                if reponse == 'n':
+                    print("Insertion annulée.")
+                elif reponse == 'y' or reponse == '':
+                    if not session.query(Livre).filter_by(isbn=product.isbn).first() :
+                        l = Livre(isbn=product.isbn, titre=product.title, auteur=', '.join(product.authors), prix=prix)
+                        session.add(l)
+                        session.commit()
+                        amout = amout + prix
+                        print("Dépense de {}€".format(amout))
+                    else:
+                        print("Déjà en db")
                 else:
-                    print("Déjà en db")
+                    print("Je ne comprend pas la réponse.")
         except Exception as ex:
             print(ex)
+
+    atexit.register(lambda : print("Exit"))
